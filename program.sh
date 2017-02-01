71980af3f0823562d7948d3a7df50099f6cc02cb
karthiktester29 <karthik29051994@gmail.com>
2017-02-01 13:18:51 -0800

    add,sub & mul operations

---
+++ program.sh
@@ -0,0 +1,11 @@
+echo "Enter three numbers to perform arithematic operations"
+read num1 
+read num2
+read num3
+echo ""
+echo Addition of given numbers is = $((num1+num2+num3))
+echo ""
+echo subtraction of given numbers is = $((num1-num2-num3))
+echo ""
+echo Multiplication of given numbers is = $((num1*num2*num3))
+echo ""
